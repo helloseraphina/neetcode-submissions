6ce164f256d28e20feeff6d217f8042eadfec84e
seraphina <47409195+helloseraphina@users.noreply.github.com>
2026-07-10 18:19:06 +0200

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # result of intervals
+        res = []
+
+        for i in range(len(intervals)):
+            # edge cases
+            # if end interval of new interval is less than start value of current interval
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            # new interval goes after current interval
+            elif newInterval[0] > intervals[i][1]:
+                # append interval we are currently at
+                # because new int not overlapping
+                res.append(intervals[i])
+            else:
+                # new interval overlap with current interval we are at
+                # merge by expanding to cover both ranges
+                # smaller start, larger end
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        
+        # if loop ends, newinterval belongs at the end
+        res.append(newInterval)
+
+        return res
